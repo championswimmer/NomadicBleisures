ee58d6efd0d44442cc60252790e349e82eacd492
Raghav Apoorv <raghav.apoorv@zomato.com>
2019-04-13 14:51:37 +0530

    Start go server

---
+++ Server/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"github.com/NomadicBleisures/Server/router"
+	"github.com/sirupsen/logrus"
+	"github.com/vrecan/death"
+	"net/http"
+	"syscall"
+)
+
+func main() {
+	r, err := router.GetRouter()
+	if err != nil {
+		logrus.Panic("STOP")
+	}
+	fmt.Print("Starting server on port 8080")
+	err = http.ListenAndServe(":8080", r)
+
+	death := death.NewDeath(syscall.SIGINT, syscall.SIGTERM)
+	death.WaitForDeath()
+}
