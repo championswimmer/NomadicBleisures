f7994b9f8e0793fe4acad7ae153335f25aa11805
Naman Dwivedi <namandwivedi14@gmail.com>
2019-04-13 17:03:59 +0530

    nomad city cards

--- Android/app/src/main/java/com/booking/nomadicbleisures/SearchFragment.kt
+++ Android/app/src/main/java/com/booking/nomadicbleisures/SearchFragment.kt
@@ -1,15 +1,48 @@
 package com.booking.nomadicbleisures
 
 import android.os.Bundle
+import android.os.Handler
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.booking.nomadicbleisures.models.NomadCity
+import com.booking.nomadicbleisures.utils.IOUtils
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+import com.squareup.picasso.Picasso
+import kotlinx.android.synthetic.main.fragment_search.*
+import kotlinx.android.synthetic.main.fragment_search.view.*
+import kotlinx.android.synthetic.main.item_city.view.*
+
 
 class SearchFragment: Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.fragment_search, container, false)
+
+        view.progressBar.visibility = View.VISIBLE
+        Handler().postDelayed({
+            fetchPopularCities()
+            view.progressBar.visibility = View.GONE
+        }, 1000)
+
         return view
+    }
+
+    private fun fetchPopularCities() {
+        val listType = object : TypeToken<List<NomadCity>>() {}.type
+        val cityList = Gson().fromJson(IOUtils.loadJSONFromAsset(activity!!, "cities.json"), listType)
+                as List<NomadCity>
+        for (city in cityList) {
+            val cityView = LayoutInflater.from(activity!!).inflate(R.layout.item_city, llCityList, false)
+            cityView.cityTitle.text = city.name
+            cityView.cityPrice.text = "$${city.price}"
+            cityView.cityWeather.text = "${city.temperature}C"
+            cityView.cityInternet.text = "${city.internetSpeed.toShort()}mbps"
+            Picasso.get().load("https://nomadlist.com${city.image}").into(cityView.cityImage);
+            llCityList.addView(cityView)
+        }
     }
 }
--- Android/app/src/main/java/com/booking/nomadicbleisures/models/NomadCity.kt
+++ Android/app/src/main/java/com/booking/nomadicbleisures/models/NomadCity.kt
@@ -5,6 +5,8 @@ import com.google.gson.annotations.SerializedName
 data class NomadCity(@SerializedName("name") val name: String,
                      @SerializedName("country")val country: String,
                      @SerializedName("internet_speed")val internetSpeed: Int,
-                     @SerializedName("air_quality_score") val airQualityScore: Int,
+                     @SerializedName("air_quality_score") val airQualityScore: Float,
                      @SerializedName("temperatureC") val temperature: String,
-                     @SerializedName("nomad_score") val nomadScore: Float)
+                     @SerializedName("nomad_score") val nomadScore: Float,
+                     @SerializedName("short_term_cost_in_usd") val price: Int,
+                     @SerializedName("image") val image: String)
