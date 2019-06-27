7306dd5f558f72e58d10808afd25a8f6f3e6f8f9
Arnav Gupta <arnav@codingblocks.com>
2019-06-27 21:23:41 +0200

    meta selections
    Signed-off-by: Arnav Gupta <arnav@codingblocks.com>

---
+++ API/routes/meta.js
@@ -0,0 +1,48 @@
+const Sequelize = require('sequelize')
+const { Op } = Sequelize
+const { Cities, BookingCities, Countries } = require('../db')
+const { Router } = require('express')
+
+const route = Router()
+
+route.get('/selections', async (req, res) => {
+
+  res.json({
+    selections: [
+      { type: 'cost',
+        title: 'Cost of Living',
+        options: [
+          {value: 'low', title: '< $25 / d'},
+          {value: 'medium', title: '$25 ~ $50 / d'},
+          {value: 'high', title: '> $50 / d'}
+        ]
+      },
+      {
+        type: 'weather',
+        title: 'Weather',
+        options: [
+          {value: 'cold', title: '❄️'},
+          {value: 'warm', title: '☀️'},
+          {value: 'mild', title: '🌥'}
+        ]
+      },
+      {
+        type: 'timezone',
+        title: 'Near timezone',
+        options: [
+          {value: '-0700', title: '-0700 (San Francisco)'},
+          {value: '-0400', title: '-0400 (New York)'},
+          {value: '0000', title: '0000 (London)'},
+          {value: '0800', title: '0800 (Singapore)'}
+        ]
+      }
+    ],
+    options: [
+      {type: 'aqi', title: 'Clean Air 🍃'},
+      {type: 'internet', title: 'Fast Internet 🔌'}
+    ]
+  })
+
+})
+
+module.exports = { route }
--- API/server.js
+++ API/server.js
@@ -6,6 +6,7 @@ const Sequelize = require('sequelize')
 const app = express()
 
 app.use('/cities', require('./routes/cities').route)
+app.use('/meta', require('./routes/meta').route)
 
 app.listen(4242, () => {
   console.log('Server started on http://localhost:4242')
