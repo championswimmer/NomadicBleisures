8b5b44495bf65bd89a3921299c46551eef8b4da8
Arnav Gupta <arnav@codingblocks.com>
2019-06-27 17:25:51 +0200

    city getter
    Signed-off-by: Arnav Gupta <arnav@codingblocks.com>

--- API/server.js
+++ API/server.js
@@ -1,9 +1,31 @@
 const express = require('express')
+const { Cities, BookingCities, Countries } = require('./db')
+const { Op } = require('sequelize')
+const Sequelize = require('sequelize')
 
 const app = express()
 
-app.get('cities')
+app.get('/cities', async (req, res) => {
+  const whereArr = []
+
+  if (req.query.name) {
+    whereArr.push(
+      Sequelize.where(
+        Sequelize.fn('lower', Sequelize.col('name')),
+        { [ Op.like ]: `%${req.query.name}%` }
+      )
+    )
+  }
+
+  const cities = await Cities.findAll({
+    where: {
+      [ Op.and ]: whereArr
+    }
+  })
+
+  res.send(cities)
+})
 
 app.listen(4242, () => {
-  console.log('Server started')
+  console.log('Server started on http://localhost:4242')
 })
